7be3d2416235c8915bd071855c19fac17722b44a
Ken <goryo.webdev@gmail.com>
2021-01-17 05:59:16 +0000

    initial

---
+++ urcisite/urcisite/urls.py
@@ -0,0 +1,61 @@
+"""urcisite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth.models import User
+# from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+# router = routers.DefaultRouter()
+# router.register(r'users', UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+#urlpatterns = [
+#    path('', TemplateView.as_view(template_name='index.html')),
+#    path('/', urcisite.frontend)
+#    path('admin/', admin.site.urls),
+#    path('', include(router.urls)),
+#    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+#]
+from django.contrib import admin
+from django.urls import path, include
+from .routers import router
+from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('article', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
